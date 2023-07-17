1dcaa0fd3d4283a57fe2eada976656d0915fecdb
lipeiquan <peiquan.li@foxmail.com>
2023-07-17 16:11:38 +0800

    add README

--- README.md
+++ README.md
@@ -1,5 +1,34 @@
 # Vite-plugin-mail
 需配合*vite-plugin-zip-output*等压缩插件使用，在构建静态文件并压缩成zip后， 将zip文件发送到邮箱中
+
+## 使用
+```sh
+npm install vite-plugin-mail -D 
+```
+
+```js
+import {defineConfig} from 'vite'
+import VitePluginMail from 'vite-plugin-mail'
+
+export default defineConfig({
+  plugins:[
+    VitePluginMail({
+      user: 'xxx@qq.com',
+      pass: 'xxx',
+      file: 'xxx',
+    })
+  ]
+})
+```
+
+## 参数
+|参数|必填|说明|
+|--|--|--|
+|user|√|QQ邮箱账号|
+|pass|√|对应账号的授权码，获取方法见下|
+|file|√|要发送的文件地址|
+|to|x|接收者邮箱，不传默认使用user的邮箱为接收者|
+
 ## 授权码
 目前仅支持qq邮箱
 ### QQ邮箱
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import{ Plugin, ResolvedConfig, normalizePath} from 'vite'
-import type {Options} from './type'
+import { Plugin, ResolvedConfig, normalizePath } from 'vite'
+import type { Options } from './type'
 import Mail from './mail'
-import {resolve} from 'node:path'
-import {existsSync} from 'node:fs'
+import { resolve } from 'node:path'
+import { existsSync } from 'node:fs'
 import chalk from 'chalk'
 
 export default function VitePluginMail({ user, pass, to, file }: Options): Plugin {
@@ -12,31 +12,27 @@ export default function VitePluginMail({ user, pass, to, file }: Options): Plugi
     name: 'vite-plugin-mail',
     apply: 'build',
     configResolved(resolvedConfig: ResolvedConfig) {
-      const {root} = resolvedConfig
+      const { root } = resolvedConfig
       path = normalizePath(resolve(root, file))
     },
     async closeBundle() {
-      console.log('vite-plugin-mail: 开始执行closeBundle');
-      console.log('path: ', path);
-      
-      if (!existsSync(path)) {
-        console.log(`vite-plugin-mail: ${chalk.red(path, '找不到文件！')}`);
-        return
+      try {
+        console.log('>>>vite-plugin-mail: 开始执行。附件地址为', path)
+        if (!existsSync(path)) {
+          console.log(`>>>vite-plugin-mail: ${chalk.red(path, '找不到文件！')}`)
+          return
+        }
+        const mail = new Mail({ user, pass })
+        await mail.send({
+          to: to || user,
+          subject: 'VitePluginMail自动发送',
+          attachments: [{ path }],
+        })
+        console.log(`>>>vite-plugin-mail: ${chalk.green('邮件发送成功！')}`)
+      } catch (err) {
+        console.log(`>>>vite-plugin-mail: ${chalk.red('邮件发送失败！')}`)
+        console.log(err)
       }
-      const mail = new Mail({user, pass})
-      await mail.send({
-        to: to || user,
-        subject: 'VitePluginuser自动发送',
-        attachments: [
-          {
-            path
-          }
-        ]
-      })
-      console.log(`vite-plugin-mail: ${chalk.green('邮件发送成功！')}`);
-      
     },
   }
 }
-
-
